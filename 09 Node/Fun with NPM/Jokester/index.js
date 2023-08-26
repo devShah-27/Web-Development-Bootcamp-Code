e824905f03941159bec9ffadd1359e5f307507e5
Dev Shah <109164083+devShah-27@users.noreply.github.com>
2023-08-26 12:57:11 +0530

    Understand package.json file

--- 09 Node/Fun with NPM/Jokester/index.js
+++ 09 Node/Fun with NPM/Jokester/index.js
@@ -1,8 +1,10 @@
-const dadJoke = require("give-me-a-joke");
+const dadJoke = require('give-me-a-joke');
+const colors = require('colors');
 
 dadJoke.getRandomDadJoke((joke) => {
-    console.log(joke);
+    console.log(joke.trap.rainbow);
 });
 
 
 // console.dir(dadJoke);
+// console.dir(rainbow);
