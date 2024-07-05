30be228ecdb9a132357566bccf19b7a819986afc
Dev Shah <shahdev149@gmail.com>
2024-07-05 08:53:01 +0530

    learn basics of error handling in express

--- 16 Express-Error-Handling/async-errors/index.js
+++ 16 Express-Error-Handling/async-errors/index.js
@@ -23,60 +23,96 @@ app.use(methodOverride("_method"));
 
 const categories = ["Fruit", "Vegetable", "Dairy"];
 
-app.get("/products", async (req, res) => {
-  const { category } = req.query;
-  if (category) {
-    const products = await Product.find({ category });
-    res.render("products/index", { products, category });
-  } else {
-    const products = await Product.find({});
-    res.render("products/index", { products, category: "All" });
-  }
-});
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((e) => next(e));
+  };
+}
+
+app.get(
+  "/products",
+  wrapAsync(async (req, res, next) => {
+    const { category } = req.query;
+    if (category) {
+      const products = await Product.find({ category });
+      res.render("products/index", { products, category });
+    } else {
+      const products = await Product.find({});
+      res.render("products/index", { products, category: "All" });
+    }
+  })
+);
 
 app.get("/products/new", (req, res) => {
   // throw new appError('Not Allowed', 401)
   res.render("products/new", { categories });
 });
 
-app.post("/products", async (req, res) => {
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  console.log(newProduct);
-  res.redirect(`/products/${newProduct._id}`);
-});
+app.post(
+  "/products",
+  wrapAsync(async (req, res, next) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+  })
+);
 
-app.get("/products/:id", async (req, res, next) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    return next(new appError("Product not found", 404));
+app.get(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      throw new appError("Product not found", 404);
+    }
+    res.render("products/show", { product });
+  })
+);
+
+app.get(
+  "/products/:id/edit",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+      throw new appError("Product not found", 404);
+    }
+    res.render("products/edit", { product, categories });
+  })
+);
+
+app.put(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/products/${updatedProduct._id}`);
+  })
+);
+
+app.delete(
+  "/products/:id",
+  wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    console.log(`Delete request for ${id}`);
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+  })
+);
+
+const handleValidation = (err) => {
+  console.dir(err);
+  return new appError(`Validation Failed... ${err}`, 400);
+};
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.type === "ValidationError") {
+    err = handleValidation(err);
   }
-  res.render("products/show", { product });
-});
-
-app.get("/products/:id/edit", async (req, res, next) => {
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  if (!product) {
-    return next(new appError("Product not found", 404));
-  }
-  res.render("products/edit", { product, categories });
-});
-
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/products/${updatedProduct._id}`);
-});
-
-app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  console.log(`Delete request for ${id}`);
-  await Product.findByIdAndDelete(id);
-  res.redirect("/products");
+  next();
 });
 
 app.use((err, req, res, next) => {
